eddfa3aa340780734ddccc2efe7cc87809e419bb
Mayson <maysongreen@u.boisestate.edu>
2018-04-29 01:01:03 -0600

    -"DDL changes"

--- schema.sql
+++ schema.sql
@@ -1,15 +1,19 @@
+CREATE database Tasks;
+
+use Tasks;
+
 CREATE TABLE task (id INT AUTO_INCREMENT,
                         label VARCHAR(50) NOT NULL,
                         create_date DATE NOT NULL,
                         due_date DATE,
-                        status INT DEFAULT 0
+                        status INT DEFAULT 0,
                         PRIMARY KEY (id) );
 
 CREATE TABLE tag (name VARCHAR(25),
                     PRIMARY KEY (name) );
 
-CREATE TABLE tagged_task (task_id,
-                        tag_name,
+CREATE TABLE tagged_task (task_id int,
+                        tag_name varchar(25),
                         FOREIGN KEY (task_id) REFERENCES task(id),
                         FOREIGN KEY (tag_name) REFERENCES tag(name),
                         PRIMARY KEY (task_id, tag_name));
