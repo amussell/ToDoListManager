7fc0e8720b04a16385c2011db4ff5bdabcdb8ed4
Mayson <maysongreen@u.boisestate.edu>
2018-04-30 19:40:11 -0600

    -"Added option to enter credentials through the command line so passwords are not visible when typing"

--- src/main/java/ToDoListManager.java
+++ src/main/java/ToDoListManager.java
@@ -1,3 +1,5 @@
+import java.io.Console;
+
 /**
  * Driving class for our Task Manager.
  * Parses the command line args and starts a database session
@@ -10,24 +12,46 @@ public class ToDoListManager {
         String sandboxId;
         String sandboxPassword;
         int dbPort;
+
         Credentials credentials = null; //Credentials used for SSH and the DataBase
         try {
             sshId = args[0];
-            sshPassword = args[1];
-            sandboxId = args[2];
-            sandboxPassword = args[3];
-            dbPort = Integer.parseInt(args[4]);
-
-            credentials = new Credentials(sshId, sshPassword, sandboxId, sandboxPassword, dbPort);
-
+            if(sshId.equals("c")){ //Will read in credentials from the command line if 'c' is used as an argument
+                credentials = readCredentialsFromConsole();
+            } else{
+                sshPassword = args[1];
+                sandboxId = args[2];
+                sandboxPassword = args[3];
+                dbPort = Integer.parseInt(args[4]);
+                credentials = new Credentials(sshId, sshPassword, sandboxId, sandboxPassword, dbPort);
+            }
         } catch(Exception e) {
-            e.printStackTrace();
             printUsage();
             System.exit(1);
         }
 
         TaskManager tm = new TaskManager(credentials);
         tm.run();
+    }
+
+    /**
+     * Reads in credentials from the console
+     * @return
+     */
+    private static Credentials readCredentialsFromConsole(){
+        Credentials credentials = null;
+        try{
+            Console console = System.console();
+            String sshUser = console.readLine("SSH Username: ");
+            String sshPassword = new String(console.readPassword("SSH Password: "));
+            String dbPassword = new String(console.readPassword("DB Password: "));
+            String dbPort = console.readLine("DB Port: ");
+            credentials = new Credentials(sshUser, sshPassword, "msandbox", dbPassword,Integer.parseInt(dbPort));
+        } catch (Exception e){
+            System.out.println("Unable to get a Console. Exiting program.");
+            System.exit(0);
+        }
+            return credentials;
     }
 
     public static void printUsage() {
